aa9de7aab09f0fa2878b1c3743c764abe3e77680
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-03-12 16:09:17 +0100

    Home

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,50 @@
 <div class="footer">
     <div class="footer-1">
         <img src="{{asset('img/logo.png')}}" alt="">
+        <ul>
+            <li>Ragione sociale: La Molisana S.P.A</li>
+            <li>Sede legale: Contrada Colle delle Alpi, 100/A</li>
+            <li>86100 - Campobasso (CB)</li>
+            <li>Pec: lamolisana@pec.it</li>
+            <li>Tel: +39 0874 4981</li>
+            <li>Fax: +39 0874 629584</li>
+            <li>info@lamolisana.it (per segnalazioni degli utenti)</li>
+            <li>commerciale@lamolisana.it</li>
+            <li>export@lamolisana.it</li>
+            <li>numero verde: 800818081</li>
+            <li>telefono: 3801292455</li>
+        </ul>
+    </div>
+    <div class="footer-2">
+        <h2>PASTIFICIO</h2>
+        <ul>
+            <li>Il Pastificio</li>
+            <li>Grano decorticato a pietra</li>
+            <li>Il Molise c'è</li>
+            <li>Filiera Integrata</li>
+            <li>100 anni di pasta</li>
+            <li>Sartoria della pasta</li>
+            <li>Spaghetto Quadrato</li>
+            <li>Le Persone</li>
+        </ul>
+        <h2>PRODOTTI</h2>
+        <ul>
+            <li>Le Classiche</li>
+            <li>Le Integrali</li>
+            <li>Le Speciali</li>
+            <li>Le Biologiche</li>
+            <li>Le Gluten-Free</li>
+            <li>Le Semole</li>
+            <li>Le Extra di Lusso</li>
+        </ul>
+    </div>
+    <div class="footer-3">
+        <h2>COLLEZIONE DA CHEF</h2>
+        <ul>
+            <li>Collezione da Chef</li>
+            <li>Grandi Cucine</li>
+            <li>Biologiche</li>
+            <li>Quadrate</li>
+        </ul>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $pasta = config('pasta');
+    return view('home', [
+        'pastaList' => $pasta
+    ]);
 });
 
-Route::get('/product/{id}', function ($id) {
-    return view('product',
-        ['id' => $id]
-    );
+Route::get('/product/{id?}', function ($id=null) {
+    $pasta = config('pasta');
+    return view('product',[
+        'id' => $id,
+        'pastaList' => $pasta
+        ]);
 });
